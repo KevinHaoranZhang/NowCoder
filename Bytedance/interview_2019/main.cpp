3bb1434526677c5b7c7d9898e1cfc9ee51566462
zhanghaoran03 <zhanghaoran03@baidu.com>
2021-03-31 19:48:56 +0800

    [Bytedance][2019] Q2

--- Bytedance/interview_2019/main.cpp
+++ Bytedance/interview_2019/main.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-/* question 1
+typedef long long LL;
+/* Question 1
  * @ Description:
  *
  * 我叫王大锤，是一家出版社的编辑。我负责校对投稿来的英文稿件，这份工作非常烦人，因为每天都要去修正无数的拼写错误。但是，优秀的人总能在平凡的工作中发现真理。我发现一个发现拼写错误的捷径：
@@ -33,20 +34,64 @@ using namespace std;
 
 string word_processor(string word);
 
+/* Question 2
+ * @ Description
+ *
+ * 我叫王大锤，是一名特工。我刚刚接到任务：在字节跳动大街进行埋伏，抓捕恐怖分子孔连顺。和我一起行动的还有另外两名特工，我提议
+ * 1. 我们在字节跳动大街的N个建筑中选定3个埋伏地点。
+ * 2. 为了相互照应，我们决定相距最远的两名特工间的距离不超过D。
+ *
+ * 我特喵是个天才! 经过精密的计算，我们从X种可行的埋伏方案中选择了一种。这个方案万无一失，颤抖吧，孔连顺！
+ * ……
+ * 万万没想到，计划还是失败了，孔连顺化妆成小龙女，混在cosplay的队伍中逃出了字节跳动大街。只怪他的伪装太成功了，就是杨过本人来了也发现不了的！
+ *
+ * 请听题：给定N（可选作为埋伏点的建筑物数）、D（相距最远的两名特工间的距离的最大值）以及可选建筑的坐标，计算在这次行动中，大锤的小队有多少种埋伏选择.
+ * 注意：
+ * 1. 两个特工不能埋伏在同一地点
+ * 2. 三个特工是等价的：即同样的位置组合(A, B, C) 只算一种埋伏方法，不能因“特工之间互换位置”而重复使用
+ *
+ * @ Input Example:
+ * 第一行包含空格分隔的两个数字 N和D(1 ≤ N ≤ 1000000; 1 ≤ D ≤ 1000000)
+ * 第二行包含N个建筑物的的位置，每个位置用一个整数（取值区间为[0, 1000000]）表示，从小到大排列（将字节跳动大街看做一条数轴）
+ *
+ * 4 3
+ * 1 2 3 4
+ *
+ * @ Output Example:
+ * 一个数字，表示不同埋伏方案的数量。结果可能溢出，请对 99997867 取模
+ *
+ * 4
+ * 可选方案 (1, 2, 3), (1, 2, 4), (1, 3, 4), (2, 3, 4)
+ */
+#define OVERFLOW_LIMIT 99997867
+long long num_combination(vector<int> building_list, int distance);
+
 int main() {
     // question 1
-    int total_word_num = 0;
-    cin >> total_word_num;
-    vector<string> word_list;
-    string word;
-    for (int i = 0; i < total_word_num; i++) {
-        cin >> word;
-        word_list.push_back(word);
+//    int total_word_num = 0;
+//    cin >> total_word_num;
+//    vector<string> word_list;
+//    string word;
+//    for (int i = 0; i < total_word_num; i++) {
+//        cin >> word;
+//        word_list.push_back(word);
+//    }
+//    for (int i = 0; i < word_list.size(); i++) {
+//        string processed_word = word_processor(word_list[i]);
+//        cout << processed_word << endl;
+//    }
+
+    // question 2
+    int total_building_num = 0, distance = 0;
+    cin >> total_building_num >> distance;
+    vector<int> building_list;
+    int building_location;
+    for (int i = 0; i < total_building_num; i++) {
+        cin >> building_location;
+        building_list.push_back(building_location);
     }
-    for (int i = 0; i < word_list.size(); i++) {
-        string processed_word = word_processor(word_list[i]);
-        cout << processed_word << endl;
-    }
+    cout << num_combination(building_list, distance) << endl;
+
     return 0;
 }
 
@@ -74,5 +119,24 @@ string word_processor(string word) {
         processed_word += word[i];
     }
     return processed_word;
+}
+
+// question 2
+long long num_combination(vector<int> building_list, int distance) {
+    int i, j, building_num = building_list.size();
+    long long sum = 0;
+    for (i = 0, j = 2; i < building_num - 2; i++) {
+        // find the next allowed building position
+        while (j < building_num && building_list[j] - building_list[i] <= distance) {
+            j++;
+        }
+        // combination: base (j - i - 1), choose 2 (fixed position for i)
+        // C(n, 2) = n * (n - 1) / 2
+        if (j - i - 1>= 2) {
+            sum += (long long)(j - i - 1) * (long long)(j - i - 2) / 2;
+        }
+
+    }
+    return sum % OVERFLOW_LIMIT;
 }
 
