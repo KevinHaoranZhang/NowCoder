547263cd0ee150b93090fbfc3ad193e19c67ac44
zhanghaoran03 <zhanghaoran03@baidu.com>
2021-04-01 20:35:17 +0800

    [Bytedance][2019] Q3

--- Bytedance/interview_2019/main.cpp
+++ Bytedance/interview_2019/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -31,7 +32,7 @@ typedef long long LL;
  * hello
  * woow
  */
-
+void question1();
 string word_processor(string word);
 
 /* Question 2
@@ -63,39 +64,72 @@ string word_processor(string word);
  * 4
  * 可选方案 (1, 2, 3), (1, 2, 4), (1, 3, 4), (2, 3, 4)
  */
+void question2();
 #define OVERFLOW_LIMIT 99997867
 long long num_combination(vector<int> building_list, int distance);
 
+/* Question 3
+ * @ Description
+ *
+ * 小包最近迷上了一款叫做雀魂的麻将游戏，但是这个游戏规则太复杂，小包玩了几个月了还是输多赢少。
+ * 于是生气的小包根据游戏简化了一下规则发明了一种新的麻将，只留下一种花色，并且去除了一些特殊和牌方式（例如七对子等），具体的规则如下：
+ *
+ * 总共有36张牌，每张牌是1~9。每个数字4张牌。
+ * 你手里有其中的14张牌，如果这14张牌满足如下条件，即算作和牌
+ * 14张牌中有2张相同数字的牌，称为雀头。
+ * 除去上述2张牌，剩下12张牌可以组成4个顺子或刻子。顺子的意思是递增的连续3个数字牌（例如234,567等），刻子的意思是相同数字的3个数字牌（例如111,777）
+ *
+ * 例如：
+ * 1 1 1 2 2 2 6 6 6 7 7 7 9 9 可以组成1,2,6,7的4个刻子和9的雀头，可以和牌
+ * 1 1 1 1 2 2 3 3 5 6 7 7 8 9 用1做雀头，组123,123,567,789的四个顺子，可以和牌
+ * 1 1 1 2 2 2 3 3 3 5 6 7 7 9 无论用1 2 3 7哪个做雀头，都无法组成和牌的条件。
+ *
+ * 现在，小包从36张牌中抽取了13张牌，他想知道在剩下的23张牌中，再取一张牌，取到哪几种数字牌可以和牌。
+ *
+ * @ Input Example:
+ * 输入只有一行，包含13个数字，用空格分隔，每个数字在1~9之间，数据保证同种数字最多出现4次。
+ *
+ * 1 1 1 2 2 2 5 5 5 6 6 6 9
+ *
+ * @ Output Example:
+ * 输出同样是一行，包含1个或以上的数字。代表他再取到哪些牌可以和牌。若满足条件的有多种牌，请按从小到大的顺序输出。若没有满足条件的牌，请输出一个数字0
+ *
+ * 9
+ * 可以组成1,2,6,7的4个刻子和9的雀头
+ */
+
+void question3();
+vector<int> available_num(vector<int> input_card_list, vector<int> card_counter);
+bool is_win(vector<int> remaining_card_list);
+
 int main() {
     // question 1
-//    int total_word_num = 0;
-//    cin >> total_word_num;
-//    vector<string> word_list;
-//    string word;
-//    for (int i = 0; i < total_word_num; i++) {
-//        cin >> word;
-//        word_list.push_back(word);
-//    }
-//    for (int i = 0; i < word_list.size(); i++) {
-//        string processed_word = word_processor(word_list[i]);
-//        cout << processed_word << endl;
-//    }
+    // question1();
 
     // question 2
-    int total_building_num = 0, distance = 0;
-    cin >> total_building_num >> distance;
-    vector<int> building_list;
-    int building_location;
-    for (int i = 0; i < total_building_num; i++) {
-        cin >> building_location;
-        building_list.push_back(building_location);
-    }
-    cout << num_combination(building_list, distance) << endl;
+    // question2();
 
+    // question 3
+    question3();
     return 0;
 }
 
 // question 1
+void question1() {
+    int total_word_num = 0;
+    cin >> total_word_num;
+    vector<string> word_list;
+    string word;
+    for (int i = 0; i < total_word_num; i++) {
+        cin >> word;
+        word_list.push_back(word);
+    }
+    for (int i = 0; i < word_list.size(); i++) {
+        string processed_word = word_processor(word_list[i]);
+        cout << processed_word << endl;
+    }
+}
+
 string word_processor(string word) {
     // base condition
     if (word.size() < 3) {
@@ -122,6 +156,18 @@ string word_processor(string word) {
 }
 
 // question 2
+void question2() {
+    int total_building_num = 0, distance = 0;
+    cin >> total_building_num >> distance;
+    vector<int> building_list;
+    int building_location;
+    for (int i = 0; i < total_building_num; i++) {
+        cin >> building_location;
+        building_list.push_back(building_location);
+    }
+    cout << num_combination(building_list, distance) << endl;
+}
+
 long long num_combination(vector<int> building_list, int distance) {
     int i, j, building_num = building_list.size();
     long long sum = 0;
@@ -138,5 +184,94 @@ long long num_combination(vector<int> building_list, int distance) {
 
     }
     return sum % OVERFLOW_LIMIT;
+}
+
+// question 3
+void question3() {
+    // initial 13 cards given
+    vector<int> input_card_list(13);
+    // counter for each card's occurance, maximum 4, index range 1 - 9
+    vector<int> card_counter(10, 0);
+    for (int i = 0; i < 13; i++) {
+        cin >> input_card_list[i];
+        // increment card counter accordingly
+        card_counter[input_card_list[i]]++;
+    }
+    vector<int> available_num_list = available_num(input_card_list, card_counter);
+    if (available_num_list.empty()) {
+        cout << "0" << endl;
+    } else {
+        for (int i = 0; i < available_num_list.size(); i++) {
+            cout << available_num_list[i] << " ";
+        }
+    }
+}
+
+vector<int> available_num(vector<int> input_card_list, vector<int> card_counter) {
+    // store all possible card to win
+    vector<int> available_num_list;
+    // sort input card list for winning condition check algorithm to work
+    sort(input_card_list.begin(), input_card_list.end());
+    // iterate over all possible card
+    for (int i = 1; i <= 9; i++) {
+        // skip if a card reach maximum
+        if (card_counter[i] == 4) {
+            continue;
+        }
+        // find next position to insert i
+        auto it = lower_bound(input_card_list.begin(), input_card_list.end(), i);
+        input_card_list.insert(it, i);
+
+        // check if this card can win
+        if (is_win(input_card_list)) {
+            available_num_list.push_back(i);
+        }
+
+        // remove card i from the list to continue
+        it = lower_bound(input_card_list.begin(), input_card_list.end(), i);
+        input_card_list.erase(it);
+    }
+    return available_num_list;
+}
+
+bool is_win(vector<int> remaining_card_list) {
+    // base condition
+    if (remaining_card_list.empty()) {
+        return true;
+    }
+    // check number of repeated first number in the list
+    int first_card_count = count(remaining_card_list.begin(), remaining_card_list.begin() + 4, remaining_card_list[0]);
+
+    // condition 1: bird head, take first two repeated num
+    // 14 is not divisible by 3, but 14 - 2 can
+    if (remaining_card_list.size() % 3 != 0 && first_card_count >= 2) {
+        // take out first two cards
+        if (is_win(vector<int> (remaining_card_list.begin() + 2, remaining_card_list.end()))) {
+            return true;
+        }
+    }
+
+    // condition 2: triple duplicates
+    if (first_card_count >= 3) {
+        // take out first three cards
+        if (is_win(vector<int> (remaining_card_list.begin() + 3, remaining_card_list.end()))) {
+            return true;
+        }
+    }
+
+    // condition 3: flush
+    if (count(remaining_card_list.begin(), remaining_card_list.end(), remaining_card_list[0] + 1) > 0
+        && count(remaining_card_list.begin(), remaining_card_list.end(), remaining_card_list[0] + 2) > 0) {
+        // remove the flush from the remaining card list
+        vector<int> removed_flush_list(remaining_card_list.begin() + 1, remaining_card_list.end());
+        removed_flush_list.erase(find(removed_flush_list.begin(), removed_flush_list.end(), remaining_card_list[0] + 1));
+        removed_flush_list.erase(find(removed_flush_list.begin(), removed_flush_list.end(), remaining_card_list[0] + 2));
+        if (is_win(removed_flush_list)) {
+            return true;
+        }
+    }
+
+    // no condition satisfied, no win
+    return false;
 }
 
