2701dd8bc7579e4bb195f4c9b6f9e41d275eb674
KevinHaoranZhang <zhr9118@gmail.com>
2021-04-09 19:26:52 +0800

    [Bytedance][2019] Q7

--- Bytedance/interview_2019/main.cpp
+++ Bytedance/interview_2019/main.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <map>
-
+#include <math.h>
 using namespace std;
 
 /* Question 1
@@ -189,6 +189,29 @@ void dp_tsp(vector<vector<int>> city_dis);
 
 void question6();
 
+/* Question 7
+ * @ Description
+ * 机器人正在玩一个古老的基于DOS的游戏。游戏中有N+1座建筑——从0到N编号，从左到右排列。编号为0的建筑高度为0个单位，编号为i的建筑的高度为H(i)个单位。
+ *
+ * 起初， 机器人在编号为0的建筑处。每一步，它跳到下一个（右边）建筑。假设机器人在第k个建筑，且它现在的能量值是E, 下一步它将跳到第个k+1建筑。它将会得到或者失去正比于与H(k+1)与E之差的能量。如果 H(k+1) > E 那么机器人就失去 H(k+1) - E 的能量值，否则它将得到 E - H(k+1) 的能量值。
+ *
+ * 游戏目标是到达第个N建筑，在这个过程中，能量值不能为负数个单位。现在的问题是机器人以多少能量值开始游戏，才可以保证成功完成游戏？
+ *
+ * @ Input Example:
+ * 第一行输入，表示一共有 N 组数据.
+ *
+ * 第二个是 N 个空格分隔的整数，H1, H2, H3, ..., Hn 代表建筑物的高度
+ *
+ * 5
+ * 3 4 3 2 4
+ *
+ * @ Output Example:
+ * 输出一个单独的数表示完成游戏所需的最少单位的初始能量
+ *
+ * 4
+ */
+void question7();
+
 int main() {
     // question 1
     // question1();
@@ -206,8 +229,10 @@ int main() {
     // question5();
 
     // quetion6
-    question6();
+    // question6();
 
+    // question7
+    question7();
     return 0;
 }
 
@@ -502,4 +527,20 @@ void question6() {
         change %= coin_list[i];
     }
     cout << num_coin << endl;
+}
+
+// question 7
+void question7() {
+    int building_num = 0;
+    cin >> building_num;
+    vector<int> build_heigh_list(building_num, 0);
+    for (int i = 0; i < building_num; i++) {
+        cin >> build_heigh_list[i];
+    }
+    // energy left after a build is 2E - H
+    double energy = 0.0;
+    for (int i = building_num - 1; i >= 0; i--) {
+        energy = (energy + build_heigh_list[i]) / 2.0;
+    }
+    cout <<  ceil(energy) << std::endl;
 }
