c83022b284ecc4169924ed44e6a538d623f7ce5b
KevinHaoranZhang <zhr9118@gmail.com>
2021-04-27 20:08:39 +0800

    [Tencent][2020] Q2

--- Tencent/2020/main.cpp
+++ Tencent/2020/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
+#include <stack>
 using namespace std;
 /* Question 1
  * @ Description
@@ -22,9 +23,32 @@ using namespace std;
 // question 1
 string compress(string str);
 
+/* Question 2
+ * @ Description
+ * 小Q在周末的时候和他的小伙伴来到大城市逛街，一条步行街上有很多高楼，共有n座高楼排成一行。
+ * 小Q从第一栋一直走到了最后一栋，小Q从来都没有见到这么多的楼，所以他想知道他在每栋楼的位置处能看到多少栋楼呢？
+ * （当前面的楼的高度大于等于后面的楼时，后面的楼将被挡住）
+ *
+ * @ Input Example
+ * [5,3,8,3,2,5]
+ *
+ * @ Output Example
+ * [3,3,5,4,4,4]
+ *
+ * @ Explanation
+ * 当小Q处于位置3时，他可以向前看到位置2,1处的楼，向后看到位置4,6处的楼，加上第3栋楼，共可看到5栋楼。
+ * 当小Q处于位置4时，他可以向前看到位置3处的楼，向后看到位置5,6处的楼，加上第4栋楼，共可看到4栋楼。
+ */
+
+// question 2
+vector<int> findBuilding(vector<int>& heights);
+
 int main() {
     // question 1
     cout << "question 1:" << compress("HG[3|B[2|CA]]F") << endl;
+    // question 2
+    vector<int> heights{5,3,8,3,2,5};
+    vector<int> seen_heights =  findBuilding(heights);
     return 0;
 }
 
@@ -62,4 +86,46 @@ string compress(string str) {
         cur_index++;
     }
     return str;
+}
+
+vector<int> findBuilding(vector<int>& heights) {
+    int num_building = heights.size();
+    vector<int> seen_heights(num_building, 1);
+    // O(n^2) too slow
+//    for (int i = 0; i < heights.size(); i++) {
+//        int cur_pos = i, left_max = 0, right_max = 0, cur_seen = 1;
+//        for (int left_index = cur_pos - 1; left_index >= 0; left_index--) {
+//            if (heights[left_index] > left_max) {
+//                left_max = heights[left_index];
+//                cur_seen++;
+//            }
+//        }
+//
+//        for (int right_index = cur_pos + 1; right_index < heights.size(); right_index++) {
+//            if (heights[right_index] > right_max) {
+//                right_max = heights[right_index];
+//                cur_seen++;
+//            }
+//        }
+//        seen_heights.push_back(cur_seen);
+//    }
+    // using stack O(n)
+    stack<int> left_stack;
+    stack<int> right_stack;
+    for (int i = 0; i < num_building; i++) {
+        // update final results
+        seen_heights[i] += left_stack.size();
+        seen_heights[num_building - i - 1] += right_stack.size();
+        // iterate left building, pop blocked ones
+        while (!left_stack.empty() && left_stack.top() <= heights[i]) {
+            left_stack.pop();
+        }
+        // iterate right building, pop blocked ones
+        while (!right_stack.empty() && right_stack.top() <= heights[num_building - i - 1]) {
+            right_stack.pop();
+        }
+        left_stack.push(heights[i]);
+        right_stack.push(heights[num_building - i - 1]);
+    }
+    return seen_heights;
 }
